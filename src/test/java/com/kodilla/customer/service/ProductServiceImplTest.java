da3943d5602682d3a87dee9692925b9939fda92b
wojciech-sak <wojciech.r.sak@gmail.com>
2021-11-08 20:21:38 +0100

    extend ProductServiceImplTest.findCustomerAccounts() test

--- src/test/java/com/kodilla/customer/service/ProductServiceImplTest.java
+++ src/test/java/com/kodilla/customer/service/ProductServiceImplTest.java
@@ -9,17 +9,18 @@ import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRun
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
-//@SpringBootTest(
-//        properties = {
-//                "stubrunner.cloud.loadbalancer.enabled=false"
-//        }
-//)
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "stubrunner.cloud.loadbalancer.enabled=false"
+        }
+)
+//@SpringBootTest
 @AutoConfigureStubRunner(
         stubsMode = StubRunnerProperties.StubsMode.LOCAL,
         ids = {
@@ -40,6 +41,9 @@ class ProductServiceImplTest {
         List<AccountDto> accounts = productService.findCustomerAccounts(customerId);
 
         //Then
-        assertEquals(2, accounts.size());
+        assertEquals(1, accounts.size());
+        assertEquals("08897810189710581776778244", accounts.get(0).getNrb());
+        assertEquals("PLN", accounts.get(0).getCurrency());
+        assertEquals(new BigDecimal("1000.00"), accounts.get(0).getAvailableFunds());
     }
 }
